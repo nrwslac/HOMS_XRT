c38fe3068f876034eae663e3eb03df540359f352
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2022-05-09 13:26:21 -0700

    ENH: add hide menu

--- homsScreens/launch_mr1l4_ui.sh
+++ homsScreens/launch_mr1l4_ui.sh
@@ -2,4 +2,4 @@
 
 source /cds/group/pcds/pyps/conda/pcds_conda
 
-`pydm --hide-nav-bar -m '{"YAXIS":"MR1L4:HOMS:MMS:YUP","XAXIS":"MR1L4:HOMS:MMS:XUP", "PITCH":"MR1L4:HOMS:MMS:PITCH", "MIRROR":"MR1L4"}' /reg/g/pcds/epics/ioc/xrt/HOMS_XRT/latest/homsScreens/mirrorScreen.py &`
+`pydm --hide-menu-bar --hide-nav-bar -m '{"YAXIS":"MR1L4:HOMS:MMS:YUP","XAXIS":"MR1L4:HOMS:MMS:XUP", "PITCH":"MR1L4:HOMS:MMS:PITCH", "MIRROR":"MR1L4"}' /reg/g/pcds/epics/ioc/xrt/HOMS_XRT/latest/homsScreens/mirrorScreen.py &`
